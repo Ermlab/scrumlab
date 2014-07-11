713656ae08e5c092625e60f3de40ebb7ddce1c52
Paweł Dawidczyk <paul.dawidczyk@gmail.com>
2014-07-11 15:14:04 +0200

    Wstępna struktura projektu

---
+++ client/scrumlab.js
@@ -0,0 +1,10 @@
+if (Meteor.isClient) {
+    Meteor.Router.add({
+ 	  '/':'dashboard',
+      '/projects':'projects',
+      '/sprints':'sprints',
+      '/taskboard':'taskboard',
+      '/backlog':'backlog',
+      '/burndown':'burndown'});
+}
+                      

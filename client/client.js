408be312884995530d96b1c0305edf0ff938f61a
Paweł Dawidczyk <paul.dawidczyk@gmail.com>
2014-07-16 15:37:56 +0200

    The first stage of the login panel

--- client/client.js
+++ client/client.js
@@ -15,4 +15,43 @@ Meteor.loginWithGitlabAccount = function (email, password, callback) {
     });
 
     console.log("Logging in as", loginRequest);
+
+};
+
+Template.masterLayout.events = {
+    'click #i_confirm': function () {
+
+        var email = i_email.value;
+
+        var password = i_password.value;
+
+        Meteor.loginWithGitlabAccount(email, password);
+
+        i_password.value = '';
+        i_email.value = '';
+
+        LoginPanel.style.visibility = "hidden";
+    },
+    'keypress #i_password': function (event) {
+
+        if (event.which == 13 || event.keyCode == 13) {
+            var email = i_email.value;
+
+            var password = i_password.value;
+
+            Meteor.loginWithGitlabAccount(email, password);
+
+            i_password.value = '';
+            i_email.value = '';
+
+            LoginPanel.style.visibility = "hidden";
+
+            return false;
+        }
+        return true;
+    },
+    'click #login-buttons-logout': function()
+    {
+        LoginPanel.style.visibility = "visible";
+    }
 };

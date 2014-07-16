979bfeb0c8ba02a5474c924da3a3bd42c6463e9f
Mateusz Kalinowski <m.k.kalinowski@gmail.com>
2014-07-16 13:27:27 +0200

    Integrated Backlog with new layout and routing. Corrected minor typos

--- scrumlab.js
+++ scrumlab.js
@@ -1,3 +1,6 @@
 // Collections
 Projects = new Meteor.Collection("projects");
 Issues = new Meteor.Collection("issues");
+Assignees = new Meteor.Collection("assignees");
+Stories = new Meteor.Collection("stories");
+Tasks = new Meteor.Collection("tasks");

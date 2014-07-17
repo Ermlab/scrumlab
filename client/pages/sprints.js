26e518bd139e88d6dce4674ff7c95d84c050f875
Mateusz Kalinowski <m.k.kalinowski@gmail.com>
2014-07-17 15:56:09 +0200

    Added sprint estimated time recalculation on task assigning

--- client/pages/sprints.js
+++ client/pages/sprints.js
@@ -12,11 +12,10 @@ Template.sprints.rendered = function () {
     // Setting sortable property to backlog and sprint containers
     $("#backlogItems, .sprint").sortable({
         stop: function (event, ui) {
-            // Getting the element id and containing sprint's id or a backlog container
+            // Getting the element id and containing sprint's id (or a backlogItems container)
             var ownerId = ui.item.parent().attr("id");
             var selfId = ui.item.attr("id");
-            // If no owner is specified or element was returned to backlog container
-            // ownerId is set to 0
+            // If no owner is specified or element was returned to backlog container ownerId is set to 0
             if (ownerId == 'backlogItems') ownerId = '0';
             // Check if the item was dropped back in container it was taken from by
             // comparing parent id with original parent id stored in "ref" variable
@@ -26,6 +25,38 @@ Template.sprints.rendered = function () {
                         sprint: ownerId
                     }
                 });
+                // Check if owner is actually a sprint
+                if (ownerId != 0) {
+                    // Recalculate the time estimate of a sprint 
+                    var sum = 0;
+                    var data = Stories.find({
+                        sprint: ownerId
+                    }).fetch();
+                    while (data.length > 0) sum += parseInt(data.pop().time);
+                    // Update the time estimate
+                    Sprints.update(ownerId, {
+                        $set: {
+                            time: sum
+                        }
+                    });
+                }
+                // Get previous owner id to allow time estimate recalculation
+                var previousOwnerId = ui.item.attr("ref");
+                // Check if previous owner is actually a sprint
+                if (previousOwnerId != 0) {
+                    // Recalculate the time estimate of a sprint 
+                    var sum = 0;
+                    var data = Stories.find({
+                        sprint: previousOwnerId
+                    }).fetch();
+                    while (data.length > 0) sum += parseInt(data.pop().time);
+                    // Update the time estimate
+                    Sprints.update(previousOwnerId, {
+                        $set: {
+                            time: sum
+                        }
+                    });
+                }
                 // Getting rid of the doubled item
                 ui.item.remove();
             }

a37c9cc4f6c44467894874c7e02a641c75f2bcac
pgorecki <przemyslaw.gorecki@gmail.com>
2014-07-22 08:55:23 +0200

    Modified gitlab package to use local npm gitlab module

--- packages/gitlab/gitlab.js
+++ packages/gitlab/gitlab.js
@@ -1,1 +1,6 @@
-GitLab = Npm.require("gitlab");
+// I made some changes to official NPM gitlab module (v0.8.6), so for now
+// we are using a local copy of the module
+// See https://www.npmjs.org/package/gitlab
+
+// GitLab = Npm.require("gitlab");
+GitLab = Npm.require(process.env.PWD);

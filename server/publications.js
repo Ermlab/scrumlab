5e5cef60e61af9e3ddf7ca4075eb0dc6ceb1c4ed
ksopyla <sopyla@ermlab.com>
2014-07-22 15:00:09 +0200

    move publications to another file

---
+++ server/publications.js
@@ -0,0 +1,34 @@
+/*--- publish section ---*/
+
+//publish user data in order to have access to gitlab user data
+Meteor.publish("userData", function () {
+    if (this.userId) {
+        return Meteor.users.find({
+            _id: this.userId
+        }, {
+            fields: {
+                'gitlab': 1
+            }
+        });
+    } else {
+        this.ready();
+    }
+});
+
+
+//TODO: it should return only logged user projects
+Meteor.publish('userProjects', function () {
+
+    if (this.userId) {
+
+        return Projects.find({
+            member_ids: this.userId
+        });
+    } else
+        return null;
+});
+
+
+Meteor.publish('issues', function (projectId) {
+    return Issues.find();
+});

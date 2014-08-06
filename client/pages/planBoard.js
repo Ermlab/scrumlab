b64dcdc7c6869cfda0501fbea28d8a84fb6d5b77
Paweł Dawidczyk <paul.dawidczyk@gmail.com>
2014-08-06 16:50:08 +0200

    Added legend for chart Velocity

--- client/pages/planBoard.js
+++ client/pages/planBoard.js
@@ -70,59 +70,6 @@ Template.planBoardSprintsInput.rendered = function () {
         // Setting datepicker property for easy date selection
         $("#datepicker").datepicker();
     }, 500);
-
-    var data = {
-        labels: ["New sprint", "Testing spring", "Testing char"],
-        datasets: [
-            {
-                label: "Hours",
-                fillColor: "rgba(220,220,220,0.5)",
-                strokeColor: "rgba(220,220,220,0.8)",
-                highlightFill: "rgba(220,220,220,0.75)",
-                highlightStroke: "rgba(220,220,220,1)",
-                data: [21, 19, 12]
-        },
-            {
-                label: "Stories",
-                fillColor: "rgba(151,187,205,0.5)",
-                strokeColor: "rgba(151,187,205,0.8)",
-                highlightFill: "rgba(151,187,205,0.75)",
-                highlightStroke: "rgba(151,187,205,1)",
-                data: [3, 3, 3]
-        }
-    ]
-    };
-    var options = {
-        //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-        scaleBeginAtZero: true,
-
-        //Boolean - Whether grid lines are shown across the chart
-        scaleShowGridLines: true,
-
-        //String - Colour of the grid lines
-        scaleGridLineColor: "rgba(0,0,0,.05)",
-
-        //Number - Width of the grid lines
-        scaleGridLineWidth: 1,
-
-        //Boolean - If there is a stroke on each bar
-        barShowStroke: true,
-
-        //Number - Pixel width of the bar stroke
-        barStrokeWidth: 2,
-
-        //Number - Spacing between each of the X value sets
-        barValueSpacing: 5,
-
-        //Number - Spacing between data sets within X values
-        barDatasetSpacing: 1,
-
-        //String - A legend template
-        legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-    };
-    var ctx = document.getElementById("myChart").getContext("2d");
-    var barChart = new Chart(ctx).Bar(data, options);
-
 }
 
 Template.planBoardSprintsList.helpers({
